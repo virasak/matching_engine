1208fcdb81398a22a88ea63c2b8439041f713d01
Virasak Dungsrikaew <virasak@gmail.com>
2019-01-02 00:55:14 +0700

    mix app

---
+++ lib/matching_engine.ex
@@ -0,0 +1,18 @@
+defmodule MatchingEngine do
+  @moduledoc """
+  Documentation for MatchingEngine.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MatchingEngine.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/matching_engine_test.exs
@@ -0,0 +1,8 @@
+defmodule MatchingEngineTest do
+  use ExUnit.Case
+  doctest MatchingEngine
+
+  test "greets the world" do
+    assert MatchingEngine.hello() == :world
+  end
+end
